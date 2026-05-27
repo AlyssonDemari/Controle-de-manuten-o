643d633004fa6eceaf8e15f3dc10e84a2e57d47d
AlyssonDemari <alyssondemari@gmail.com>
2026-05-26 23:38:29 -0300

    protótipo 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,25 @@
+const frequencias = document.querySelectorAll(".filter__button");
+
+frequencias.forEach((botao) => {
+    botao.addEventListener('click', () => {
+        frequencias.forEach(b => b.classList.remove('active'));
+        
+        botao.classList.add('active');
+    });
+});
 
 
+const btnAbrir = document.querySelector('.add');
+const btnFechar = document.querySelector('#teste');
+const card = document.querySelector('.inputItem');
+const inputTitulo = document.querySelector('#titulo');
+
+btnAbrir.addEventListener('click', () => {
+  card.classList.remove('hidden');
+  inputTitulo.focus();
+});
+
+
+btnFechar.addEventListener('click', () => {
+  card.classList.add('hidden');
+});
